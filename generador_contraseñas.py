214c3294839338848e3fec61c091124bf80cc3ca
Juan Carlos Fernandez Calvo <132883252+JuanCcoding@users.noreply.github.com>
2023-08-25 12:10:00 +0200

    Add files via upload

---
+++ generador_contraseñas.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+import random
+import string
+
+def generar_pass():
+    pass_longitud = int(longitud.get())
+    password = ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(pass_longitud))
+    resultado_pass.delete(0, tk.END)
+    resultado_pass.insert(0, password)
+
+def index():
+    global longitud  # Declarar la variable longitud como global
+    global resultado_pass
+    principal = tk.Tk()
+    principal.title("PASSWORD GENERATOR By Juan carlos Fernandez")
+    principal.geometry("500x150")
+
+    tk.Label(principal, text="Indica la longitud de caracteres para tu contraseña:").grid(row=0, columnspan=2, padx=20, pady=10)
+    longitud = tk.Entry(principal)
+    longitud.grid(row=0, column=2, padx=10)
+
+    tk.Label(principal, text="Tu contraseña generada es:").grid(row=1, columnspan=2, padx=20, pady=10)
+    resultado_pass = tk.Entry(principal)
+    resultado_pass.grid(row=1, column=2, padx=10)
+
+    boton1 = tk.Button(principal, text="Generar", command=generar_pass)
+    boton1.grid(row=2, columnspan=3, pady=10)
+
+    principal.mainloop()
+
+index()
+
+
+
+
+
+
